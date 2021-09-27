54e4628b47f2fc2fb6b96768f515372e98b3eddd
spartaabhishek <abhishek.soni2019@vitstudent.ac.in>
2021-09-27 14:48:16 +0530

    header added

---
+++ src/components/Layout/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
+
+export default function Header() {
+    return (
+        <div>hello</div>
+    )
+}
